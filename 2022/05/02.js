bf86022bd36cff639ef32a0d42fa52f62ad04ef4
Sparisoma Viridi <dudung@gmail.com>
2022-05-02 09:04:14 +0700

    Update 02.js

--- 2022/05/02.js
+++ 2022/05/02.js
@@ -7,13 +7,54 @@
 
 function content() {
   var poem = [];
-
-  var stanza = `
-A
-B
-C
-D
-`; poem.push(stanza)
+  
+  // 1st
+  var stanza = [
+  "",
+  "",
+  "",
+  ""
+  ];
+  poem.push(stanza)
+  
+  // nth
+  var stanza = [
+  "",
+  "",
+  "",
+  ""
+  ];
+  poem.push(stanza)
+  
+  // nth
+  var stanza = [
+  "",
+  "",
+  "",
+  ""
+  ];
+  poem.push(stanza)
+  
+  var stanza = [
+  "",
+  "",
+  "",
+  ""
+  ];
+  poem.push(stanza)  
+  
+  
+  return poem;
 }
+[5/2, 07:13] Sparisoma Viridi: Sudah dua tahun berlalu
+Tanpa sholat Ied di Gasibu
+Alhamdulillah keadaan membaik
+Dapat sholat bersama lagi
+[5/2, 07:38] Sparisoma Viridi: Pusdai ke Gasibu
+Lewat Diponegoro
+Gasibu ke Pusdai
+Melalui Surapati
+
+
 
 
