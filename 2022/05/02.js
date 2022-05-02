28db775869b85807a10d12607a60a95dd71d5a0f
Sparisoma Viridi <dudung@gmail.com>
2022-05-02 18:34:38 +0700

    Update 02.js

--- 2022/05/02.js
+++ 2022/05/02.js
@@ -35,4 +35,24 @@ Kabut merengkuh bukit jauh
 Hujan rintik lalu deras
 Ciprat titik-titik hitam
 Sisa-sisa api unggun
+
+Cuci-cuci mangkuk pelastik
+Sampai tenda santap popmie
+Tambahkan rendang dan nasi
+Alhamdulillah menambah energi
+
+Kabut lebat menjelang maghrib
+Api-api unggun mulai dinyalakan
+Ke air lalu mushola
+Hujan angin kembali mengunjungi
+
+Sholat di mushola sholat di tenda
+Sholat di tenda sholat di mushola
+Berganti bergatung cuaca
+Musim panas hanya Juni-Agustus
+
+Pause machen wir jetzt?
+Tenda bocor lagi di ujung
+Belum tahu cara tangani
+Semoga hujan segera berhenti
 `;
