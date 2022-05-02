362841d872e35a41260573c845083793196d9dc3
Sparisoma Viridi <dudung@gmail.com>
2022-05-02 14:05:34 +0700

    Update 02.js
    Simplify

--- 2022/05/02.js
+++ 2022/05/02.js
@@ -5,98 +5,35 @@
   http://github.com/dudung
 */
 
-function content() {
-  var poem = [];
-  
-  // 1st
-  var stanza = [
-  "Jalan terblok mobil di Kolam",
-  "Menjaga jalan tetap lapang",
-  "Ke Pusdai ternyata Gasibu",
-  "Kecuali hujan boleh di sana"
-  ];
-  poem.push(stanza);
-  
-  // 2nd
-  var stanza = [
-  "Sudah dua tahun berlalu",
-  "Tanpa sholat Ied di Gasibu",
-  "Alhamdulillah keadaan membaik",
-  "Dapat sholat bersama lagi"
-  ];
-  poem.push(stanza);
+var content =`
+Jalan terblok mobil di Kolam
+Menjaga jalan tetap lapang
+Ke Pusdai ternyata Gasibu
+Kecuali hujan boleh di sana
 
-  // 3rd
-  var stanza = [
-  "Pusdai ke Gasibu",
-  "Lewat Diponegoro",
-  "Gasibu ke Pusdai",
-  "Melalui Surapati"
-  ];
-  poem.push(stanza);
-  
-  // 4th
-  var stanza = [
-  "Belum tiba di Rancaupas",
-  "Masih empat kilo lagi",
-  "Mampir Indomaret dulu",
-  "Makan bekal menunggu buka"
-  ];
-  poem.push(stanza);
-  
-  // 5th
-  var stanza = [
-  "Lebih sedikit jam sembilan",
-  "Perut sudah bergemuruh",
-  "Sambil santap bekal sarapan",
-  "Sudah bisa beli minuman"
-  ];
-  poem.push(stanza);
-  
-  // 6th
-  var stanza = [
-  "Kabut merengkuh bukit jauh",
-  "Hujan rintik lalu deras",
-  "Ciprat titik-titik hitam",
-  "Sisa-sisa api unggun"
-  ];
-  poem.push(stanza);
-  
-  // nth
-  var stanza = [
-  "",
-  "",
-  "",
-  ""
-  ];
-  poem.push(stanza);
-    
-  // nth
-  var stanza = [
-  "",
-  "",
-  "",
-  ""
-  ];
-  poem.push(stanza);
-  
-  // nth
-  var stanza = [
-  "",
-  "",
-  "",
-  ""
-  ];
-  poem.push(stanza);
-  
-  // nth
-  var stanza = [
-  "",
-  "",
-  "",
-  ""
-  ];
-  poem.push(stanza);
-   
-  return poem;
+Sudah dua tahun berlalu
+Tanpa sholat Ied di Gasibu
+Alhamdulillah keadaan membaik
+Dapat sholat bersama lagi
+
+Pusdai ke Gasibu
+Lewat Diponegoro
+Gasibu ke Pusdai
+Melalui Surapati
+
+Belum tiba di Rancaupas
+Masih empat kilo lagi
+Mampir Indomaret dulu
+Makan bekal menunggu buka
+
+Lebih sedikit jam sembilan
+Perut sudah bergemuruh
+Sambil santap bekal sarapan
+Sudah bisa beli minuman
+
+Kabut merengkuh bukit jauh
+Hujan rintik lalu deras
+Ciprat titik-titik hitam
+Sisa-sisa api unggun
+`;
 }
