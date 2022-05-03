e296be9acd05519082fd53c598ee8807e539e0e1
Sparisoma Viridi <dudung@gmail.com>
2022-05-03 20:18:22 +0700

    Create 03.js

---
+++ 2022/05/03.js
@@ -0,0 +1,43 @@
+/*
+  2022_05_03.js
+  Anything hit the day
+  Sparisoma Viridi
+  http://github.com/dudung
+*/
+
+poem["2022-05-03"] =`
+Sembari udara dingin menerpa
+Dalam tenda dingin sedikit basah
+Angin menderu mencekam dingin
+Masih terdengar cekcek cecak
+
+Jauh sebelum subuh terbangun
+Dingin dilawan dan ke air
+Lanjut tumpuk kayu bakar
+Mulai menyulut api unggun
+
+Tertangkap pandanan mata
+Suatu bergerak cepat di atas matras
+Sigap cepat menghantam tepat
+Cecak kecil hitam tergodam
+
+Di tengah padang rumput
+Lembab dan dingin udaranya
+Bukan tempat hewan berdarah dingin
+Dari mana asal penyuara cekcek?
+
+Wortel dicari rusa-rusa
+Menyusuri Kabin dan Iglo
+Foto-foto di kawah putih
+Sayangnya Situ Patenggang Penuh
+
+Bismillah masuh ke hunian
+Termonitor lagi cecak kecil hitam bercak
+Boken berkelebat tepat
+Tak lama meow Kelan pun muncul
+
+Cecak di dua tempat
+Juga lintah di ponco
+Apakah kebetulan belaka?
+Atau masih merupakan serangan?
+`;
