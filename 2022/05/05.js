18749ae602fff89819c1ae516336a41334d09e8a
Sparisoma Viridi <dudung@gmail.com>
2022-05-05 23:48:00 +0700

    Create 05.js

---
+++ 2022/05/05.js
@@ -0,0 +1,28 @@
+/*
+  2022_05_05.js
+  Anything hit the day
+  Sparisoma Viridi
+  http://github.com/dudung
+*/
+
+poem["2022-05-05"] =`
+Kemarin tak tercatatat
+Kunjungi sanak di Gatsu
+Pagi ini cuci kontainer
+Juga bersihkan tong sampah
+
+Rempah daun lain telah layu
+Citroen grass telah berakar
+Bersihkan yang bisa didapat
+Tanam di kaleng cincau selasih
+
+Masih lima belas menit lagi
+Hari akan kemudian berganti
+id-admin-div telah rilis dua
+Versi sebelumnya disimpan jua
+
+Siap-siap rehat segera
+Lega rilis baru terkejar
+Besok upayakan level dua
+Ingin segera Bandung Raya
+`;
